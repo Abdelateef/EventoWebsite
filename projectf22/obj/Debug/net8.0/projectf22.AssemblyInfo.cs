a3ec09b1af586dce31034aee965bae325ebc7c33
Abdelateef Khaled <77527260+Abdelateef@users.noreply.github.com>
2024-05-18 02:36:35 +0300

    testing

--- projectf22/obj/Debug/net8.0/projectf22.AssemblyInfo.cs
+++ projectf22/obj/Debug/net8.0/projectf22.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projectf22")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba69e657b25f992a534ed738be55b68a7b26d917")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaaeaa2b108e838843f83ad6597b1c38af3646d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("projectf22")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projectf22")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
