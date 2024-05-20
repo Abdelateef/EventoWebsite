7bf448c59ec0e1ec87c46834bd9c1c53074eeabd
SeifRaslan <s-seif.raslan@zewailcity.edu.eg>
2024-05-20 15:52:36 +0300

    Fixes

--- projectf22/obj/Debug/net8.0/projectf22.AssemblyInfo.cs
+++ projectf22/obj/Debug/net8.0/projectf22.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projectf22")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+559e38cbf700117077e0dff0bb8bfa3f19226798")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e353c4d056d095b0bfbfc09d67114fa7fdef7710")]
 [assembly: System.Reflection.AssemblyProductAttribute("projectf22")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projectf22")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
