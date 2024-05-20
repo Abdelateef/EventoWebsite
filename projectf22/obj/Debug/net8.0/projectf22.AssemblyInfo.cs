b3afa3085e0ef77092bf056b623db8c7a38fdfcb
Abdallah9Abbas <s-abdallah.abbas@zewailcity.edu.eg>
2024-05-21 02:05:51 +0300

    sad
    sd

--- projectf22/obj/Debug/net8.0/projectf22.AssemblyInfo.cs
+++ projectf22/obj/Debug/net8.0/projectf22.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projectf22")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d42058cdab79009195bb9f2f071c0c1496bdbf81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31b334b737b5220246b6f32329780c24617fa779")]
 [assembly: System.Reflection.AssemblyProductAttribute("projectf22")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projectf22")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
